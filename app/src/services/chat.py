5dc431abb739021a786b91186355741937c4f927
Krittin SETDHAVANICH <krittin.set@ku.th>
2024-09-09 14:13:47 +0700

    chore: remove dup embedding

--- app/src/services/chat.py
+++ app/src/services/chat.py
@@ -92,19 +92,3 @@ class ChatCompletion:
                                                        chat_history)
         return completions_results['choices'][0]['message']['content']
 
-
-class EmbeddingsGenerator:
-    def __init__(self):
-        self.client = OpenAI(api_key=config.OPENAI_KEY)
-
-    def generate_embeddings(self, text):
-        print("Generating embeddings")
-        response = self.client.embeddings.create(
-            model="text-embedding-ada-002",
-            input=text,
-            encoding_format="float"
-        )
-        print("Done generating embeddings, dumping model")
-        embeddings = response.model_dump()
-        print("Done embedding generation")
-        return embeddings['data'][0]['embedding']
